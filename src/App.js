181073958477da117d4fa3367daa198754f8eaed
Julien <julien.larrouse@gmail.com>
2022-03-29 14:42:57 +0200

    update03

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ class App extends Component{
   render(){
     return(
     <>
-    <Joueur pseudo= "Juju" nom= "Lrs" prenom= "Julien" pdv= "10000" pm= "2000" niveau= "100"/>
-    <Joueur pseudo= "Riri" nom= "Duck" prenom= "Rigs" pdv= "1000" pm= "2000" niveau= "100"/>
-    <Joueur pseudo= "Fifi" nom= "Duck" prenom= "Ronald" pdv= "1000" pm= "2000" niveau= "100"/>
-    <Joueur pseudo= "Loulou" nom= "Duck" prenom= "Laugh" pdv= "1000" pm= "2000" niveau= "100"/>
+    <Joueur pseudo= "Juju" nom= "Lrs" prenom= "Julien" age ="34" pdv= "10000" pm= "2000" niveau= "100"/>
+    <Joueur pseudo= "Riri" nom= "Duck" prenom= "Rigs" age ="10" pdv= "1000" pm= "2000" niveau= "100"/>
+    <Joueur pseudo= "Fifi" nom= "Duck" prenom= "Ronald" age ="11" pdv= "1000" pm= "2000" niveau= "100"/>
+    <Joueur pseudo= "Loulou" nom= "Duck" prenom= "Laugh" age ="12" pdv= "1000" pm= "2000" niveau= "100"/>
     </>
     );
   }
--- src/Joueur.js
+++ src/Joueur.js
@@ -11,6 +11,7 @@ import React, {Component, Fragment} from "react";
                 <h3>Pseudo : {this.props.pseudo}</h3>
                 <h3>Nom : {this.props.nom}</h3>  
                 <h3>Prénom : {this.props.prenom}</h3>
+                <h3>Age : {this.props.age}</h3>
                 <h3>Point de vie : {this.props.pdv}</h3>
                 <h3>Point magiques : {this.props.pm}</h3>
                 <h3>Niveau : {this.props.niveau}</h3>
